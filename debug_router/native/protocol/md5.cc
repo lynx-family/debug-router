b7d444a2a15c2f84ee0fc829d795d15855b58f74
RovicnowYoyi <105358806+RovicnowYoyi@users.noreply.github.com>
2025-05-13 12:49:38 +0800

    [Optimize]Fix build warning for sprintf is deprecated
    Use snprintf instead.

--- debug_router/native/protocol/md5.cc
+++ debug_router/native/protocol/md5.cc
@@ -13,7 +13,7 @@
 #include "debug_router/native/protocol/md5.h"
 
 /* for memcpy() */
-#include <string.h>
+#include <cstring>
 /* for stupid systems */
 #include <sys/types.h>
 
@@ -36,7 +36,7 @@ std::string MD5::hexdigest() const {
   if (!finalized) return "";
 
   char txbuf[33];
-  for (int i = 0; i < 16; i++) sprintf(txbuf + i * 2, "%02x", digest[i]);
+  for (int i = 0; i < 16; i++) snprintf(txbuf + i * 2, 33, "%02x", digest[i]);
   txbuf[32] = 0;
   return std::string(txbuf);
 }
